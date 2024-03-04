4059c0d17e7169f3baf4da9f79be0076e24c0870
Shafira Fimelita Qathrunnada <97589383+shafrfq@users.noreply.github.com>
2024-03-05 00:22:45 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import babel
 from func import DataAnalyzer
 from babel.numbers import format_currency
 sns.set(style='dark')
