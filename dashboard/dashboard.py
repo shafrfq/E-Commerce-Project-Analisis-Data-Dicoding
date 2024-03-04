75e70c5b274d463da44a23e4441d8d3f7e6b823e
Shafira Fimelita Qathrunnada <97589383+shafrfq@users.noreply.github.com>
2024-03-05 01:12:15 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,7 +9,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Dataset
 datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
-all_df = pd.read_csvall_df = pd.read_csv("/dashboard/all_data.csv")
+all_df = pd.read_csvall_df = pd.read_csv("dashboard/all_data.csv")
 all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
 
