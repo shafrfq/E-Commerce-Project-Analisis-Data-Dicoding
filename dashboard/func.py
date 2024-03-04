0fc77f9590c95fab65a275ef229352ad8ae1efe5
Shafira Fimelita Qathrunnada <97589383+shafrfq@users.noreply.github.com>
2024-03-05 01:06:42 +0700

    func.py

--- dashboard/func.py
+++ dashboard/func.py
@@ -5,30 +5,6 @@ class DataAnalyzer:
     def __init__(self, df):
         self.df = df
 
-    def create_daily_orders_df(self):
-        daily_orders_df = self.df.resample(rule='D', on='order_approved_at').agg({
-            "order_id": "nunique",
-            "payment_value": "sum"
-        })
-        daily_orders_df = daily_orders_df.reset_index()
-        daily_orders_df.rename(columns={
-            "order_id": "order_count",
-            "payment_value": "revenue"
-        }, inplace=True)
-        
-        return daily_orders_df
-    
-    def create_sum_spend_df(self):
-        sum_spend_df = self.df.resample(rule='D', on='order_approved_at').agg({
-            "payment_value": "sum"
-        })
-        sum_spend_df = sum_spend_df.reset_index()
-        sum_spend_df.rename(columns={
-            "payment_value": "total_spend"
-        }, inplace=True)
-
-        return sum_spend_df
-
     def create_sum_order_items_df(self):
         sum_order_items_df = self.df.groupby("product_category_name_english")["product_id"].count().reset_index()
         sum_order_items_df.rename(columns={
